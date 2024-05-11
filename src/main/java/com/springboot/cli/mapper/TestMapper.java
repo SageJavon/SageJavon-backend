694bf741d6d81353e307b64ec53bf138e848b70b
LunarXavier <2868308846@qq.com>
2024-05-11 20:53:03 +0800

    update .gitignore

--- src/main/java/com/springboot/cli/mapper/TestMapper.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.cli.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.springboot.cli.model.DO.TestDO;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TestMapper extends BaseMapper<TestDO> {
-}
--- src/main/java/com/springboot/cli/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.springboot.cli.service;
-
-import com.springboot.cli.model.DO.TestDO;
-
-public interface TestService {
-
-    void save(TestDO testDO);
-}
